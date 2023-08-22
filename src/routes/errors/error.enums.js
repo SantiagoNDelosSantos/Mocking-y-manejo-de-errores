b74742dc9062b3e98093d4faf61873df7668b591
Santiago Nahuel De los Santos <santiagodelossantos630@gmail.com>
2023-08-22 01:27:31 -0300

    Errors middleware

--- src/routes/errors/error.enums.js
+++ src/routes/errors/error.enums.js
@@ -3,12 +3,18 @@
 const ErrorEnums = {
     // Usuarios:
 
+    /*
     ROUTING_ERROR: 1,
     INVALID_TYPES_ERROR: 2,
     DATABASE_ERROR: 3,
-
+*/
 
     // Carrito:
+    INVALID_ID_CART_ERROR: 5,
+
+
+
+
     QUANTITY_INVALID_ERROR: 4,
 
     // Productos:
